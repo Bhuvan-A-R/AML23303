c1d3e4d437db59eb4f1f3ba78406d77d3e93554c
Bhuvan A R <155255254+Bhuvan-A-R@users.noreply.github.com>
2024-09-13 12:20:14 +0530

    Add files via upload

--- Lab01.py
+++ Lab01.py
@@ -1,8 +1,10 @@
+#Develop a Python Program to Read n Digits integer number, and Separate the integer number and Display each digit.
 def seprate_digit(num):
     digits=[]
     while num>0:
         digit=num%10
-        digits.append(digit)
+        digits.insert(0,digit)
+        #digits.append(digit) //To Reverse the Digits
         num=num//10
     return digits
 
@@ -10,7 +12,7 @@ n = int(input("Enter a n-digit Input:"))
 
 digits = seprate_digit(n)
 
-##digits.reverse()
+##digits.reverse() //To Reverse the Digits
 
 for digit in digits:
     print(digit,end=" ")
